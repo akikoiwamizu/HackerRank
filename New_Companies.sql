6fb1798c60c643fcb8e850903bf6f88f15ad5fae
Akiko Iwamizu <akikoiwamizu@gmail.com>
2018-01-31 20:33:36 -0800

    Updating with correct answer

--- New_Companies.sql
+++ New_Companies.sql
@@ -1,6 +1,6 @@
 SELECT cm.company_code, cm.founder, 
-       (SELECT COUNT(lead_manager_code) FROM Lead_Manager WHERE company_code = cm.company_code),
-       (SELECT COUNT(senior_manager_code) FROM Senior_Manager WHERE company_code = cm.company_code),                              (SELECT COUNT(manager_code) FROM Manager WHERE company_code = cm.company_code),
-       (SELECT COUNT(employee_code) FROM Employee WHERE company_code = cm.company_code)
+       (SELECT COUNT(DISTINCT(lead_manager_code)) FROM Lead_Manager WHERE company_code = cm.company_code),
+       (SELECT COUNT(DISTINCT(senior_manager_code)) FROM Senior_Manager WHERE company_code = cm.company_code),                    (SELECT COUNT(DISTINCT(manager_code)) FROM Manager WHERE company_code = cm.company_code),
+       (SELECT COUNT(DISTINCT(employee_code)) FROM Employee WHERE company_code = cm.company_code)
 FROM Company AS cm
 ORDER BY cm.company_code;
